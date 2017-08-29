050feac07d9f800f662035125e9d6c51be630373
Hirdesh Kumar Pharasi <harryboy007@gmail.com>
2017-08-29 18:40:34 -0500

    Delete Liesegang.jl

--- Liesegang.jl
+++
@@ -1,9 +0,0 @@
-module Liesegang
-    include("types.jl")
-
-    export particle, box
-    export parbox_plus_H, wall_nd, aggre_sbox, nucleate_c, grap_pos, reac_box, col_box, prob_box, get_box, rotate_vec, box_vel, parts_vels!, getpos_pbc!, norm_momentum!, norm_temperature!, shift_grid!, shiftback_grid!, getpos_slip!, box_velmc, collide_mc, collide_sc
-
-    include("func_sim.jl")
-
-end
